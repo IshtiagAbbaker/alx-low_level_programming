9cf7652b5fbde8670718bacd8e66a7d4a411b790
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-08 04:00:13 -0700

    Coding

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,15 +6,15 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-  {
+{
 printf("is positive\n");
-} 
-else if (n < 0)
-  {
-printf("is negative\n"); 
 }
- else
-   {
+else if (n < 0)
+{
+printf("is negative\n");
+}
+else
+{
 printf("is zero\n");
 }
 return (0);
