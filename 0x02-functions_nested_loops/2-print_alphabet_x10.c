aa2054145bdf1a474e80123006fb08a732cc69a1
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-12 06:28:42 -0700

    Last Update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet 10 times - Entry point
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while(i<10)
+	{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	}
+	_putchar('\n');
+
+}
