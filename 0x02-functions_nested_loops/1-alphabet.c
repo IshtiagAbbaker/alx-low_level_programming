d86f4be1ec4416e919332d036fe977d1a32a6e04
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-12 06:17:34 -0700

    Last Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-#include <main.h>
+#include <stdio.h>
+#include "main.h"
 /**
  * print_alphabet - Entry point
  * Return:0
  */
-
 void print_alphabet(void)
 {
 int i = 97;
 while (i <= 122)
 {
-_putchar(i)
+_putchar(i);
 i++;
 }
 _putchar('\n');
