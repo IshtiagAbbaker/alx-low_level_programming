9bc6a9a2db9b4c4589b4706ddb7c8db42c4da0f7
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-12 10:46:04 -0700

    Last Update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
@@ -9,4 +10,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
 #endif
