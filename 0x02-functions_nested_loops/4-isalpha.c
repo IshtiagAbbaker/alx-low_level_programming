2c64f0a5701852441db91461bd68072a195b2c03
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-12 08:50:56 -0700

    Last Update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isalpha - check for lower case letter
  * @c : character to check
@@ -7,18 +8,6 @@
 
 int _isalpha(int c)
 {
-if (c >= 65 && c <= 122)
-{
-if (c == 91 || c == 92 || c == 93 || c == 94 || c == 95 || c == 96)
-{
-return (0);
-}
-else
-{
-return (1);
-}
-}
-else
-{
-return (1);
+return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90) && (c != 91) && (c != 92) && (c != 93) && (c != 94) && (c != 95) && (c != 96) && (c != 91));
+
 }
