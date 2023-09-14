b30b661e934bd482688406e3b9ff09fb76246257
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-14 05:05:20 -0700

    last Update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,16 @@
 /**
 * print_most_numbers - prints 01356789, followed by a new line
 * Return: void
-**/
+*/
 
 void print_most_numbers(void)
 {
- int c;
+char c;
 
-for (c = 0; c <= 9; c++)
+for (c = '0'; c <= '9'; c++)
 {
-if (!(c == 2 || c == 2))
-putchar(48 + c);
+if (c != '2' || c != '4')
+_putchar(c);
 }
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,10 @@ for (i = 0; i < 10; i++)
 {
 for (c = 0; c <= 14; c++)
 {
-_putchar('0' + c);
+if (j >= 10)
+_putchar('1');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
-_putchar('\n');
 }
