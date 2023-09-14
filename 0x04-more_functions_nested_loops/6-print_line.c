13c79e48cfd2b03854f7a38129d7157bc467daf3
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-14 05:46:22 -0700

    last Update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,12 @@
 void print_line(int n)
 {
 int i;
-for (i = 0; i <= n; i++)
-{
-if (i <= 0)
+if (n <= 0)
 _putchar('\n');
 else
-_putchar(95);
-}
+{
+for (i = 1; i <= n; i++)
+_putchar('_');
 _putchar('\n');
+}
 }
