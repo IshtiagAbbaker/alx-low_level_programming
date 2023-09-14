edd3211c39bf2e2b132c3b238cfff0debc6773a0
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-14 04:41:42 -0700

    last Update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,12 @@
 void more_numbers(void)
 {
 int i;
-char c;
+int c;
 for (i = 0; i < 10; i++)
 {
-for (c = '0'; c <= '14'; c++)
+for (c = 0; c <= 14; c++)
 {
-_putchar('c');
+_putchar(48 + c);
 }
 }
 _putchar('\n');
