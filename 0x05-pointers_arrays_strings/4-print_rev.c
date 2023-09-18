60c37cda651e0bc3323584c3c089a49f350358a0
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-09-18 05:30:02 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rint_rev- prints a string, in reverse, followed by a new line
+ *@s : pointer
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i = ;
+
+while (str[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
