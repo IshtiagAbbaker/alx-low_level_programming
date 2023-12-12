c8375aa187529d090277a80910b3229454035ee8
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-12-12 04:01:03 -0800

    dynamic lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files into object files
-gcc -c *.c -fPIC
-
-# Create the dynamic library liball.so
-gcc -shared -o liball.so *.o
-
-# Clean up intermediate object files
-rm *.o
+gcc *.c -c -fPIC
+gcc *.o -shared -o liball.so
 
