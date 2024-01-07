d099ef1587c4207ed4bdf6605f62037b7e61564a
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2024-01-07 07:41:43 -0800

    struc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,14 @@
-#ifndef FILE_DOG
-#define FILE_DOG
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - structure dog
- * @name: atribute pointer to char name's dog
- * @age: atribute float age's dog
- * @owner: atribute pointer to char owner's dog
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
 
 struct dog
@@ -15,15 +18,13 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
 #endif
-
