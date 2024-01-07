232d888f9ee00725d0d3f72e56dc878cbd5fe1c2
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2024-01-07 07:39:19 -0800

    struc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,10 +10,11 @@
 
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -25,3 +26,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
+
